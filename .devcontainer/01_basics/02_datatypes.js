c54a97a2375f14e83d2dea16457ef8f0f05b2b38
ShankarParki <166278059+ShankarParki@users.noreply.github.com>
2024-04-07 12:01:42 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+let name = "Shankar"
+let age = 23
+let height = 5.2
+let isLoggedIn = true
+const number = 9128374650
+let college
+
+console.table([name, age, height, isLoggedIn, number])
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof height)
+console.log(typeof isLoggedIn)
+console.log(typeof number)
+console.log(typeof college)
+console.log(typeof null)
+console.log(typeof NaN)
